1a850be451993478cfdfd7f6161e95c715f0d1b5
lanouhn <lanouhn@lanouhndemac-mini-95.local>
2016-05-15 10:35:31 +0800

    Initial Commit

---
+++ 123/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  123
+//
+//  Created by lanouhn on 16/5/15.
+//  Copyright © 2016年 liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 123/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  123
+//
+//  Created by lanouhn on 16/5/15.
+//  Copyright © 2016年 liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
